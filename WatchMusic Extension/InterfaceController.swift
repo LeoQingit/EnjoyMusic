3d162c806b852e38cae4609b65868c81dafa0143
Leo <leo_qin_@msn.com>
2019-09-27 00:16:38 +0800

    no message

--- WatchMusic Extension/InterfaceController.swift
+++ WatchMusic Extension/InterfaceController.swift
@@ -8,12 +8,15 @@
 
 import WatchKit
 import WatchConnectivity
+import CoreAudio
+import AVFoundation
 import Foundation
 
 
 class InterfaceController: WKInterfaceController {
 
     @IBOutlet weak var mainTable: WKInterfaceTable!
+    var player: AVAudioPlayer!
     
     var session: WCSession!
     
@@ -23,6 +26,8 @@ class InterfaceController: WKInterfaceController {
         session = WCSession.default
         session.delegate = self
         session.activate()
+        
+
         
         let mainTitleArr = [
             "正在播放",
@@ -58,8 +63,25 @@ extension InterfaceController: WCSessionDelegate {
     
     func session(_ session: WCSession, didReceive file: WCSessionFile) {
         print(file)
-
-        let data = Data.init(contentsOf: file.fileURL)
+        let avSession = AVAudioSession.sharedInstance()
+        
+        
+        do {
+            let data = try Data.init(contentsOf: file.fileURL)
+            try avSession.setCategory(AVAudioSession.Category.playback, mode: .default, policy: .longForm, options: [])
+            player = try AVAudioPlayer(data: data)
+            avSession.activate(options: []) { (success, error) in
+                // Check for an error and play audio.
+                
+                
+                self.player.play()
+            }
+            
+        } catch {
+            
+        }
+        
+        
     }
     func session(_ session: WCSession, didReceiveMessageData messageData: Data) {
         print(messageData)
