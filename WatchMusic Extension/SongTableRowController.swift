da0985cf84e9292bc53f1bccae510106eced3378
Leo <leo_qin_@msn.com>
2019-09-30 00:09:21 +0800

    no message

---
+++ WatchMusic Extension/SongTableRowController.swift
@@ -0,0 +1,13 @@
+//
+//  SongTableRowController.swift
+//  WatchMusic Extension
+//
+//  Created by Qin Leo on 2019/9/29.
+//  Copyright © 2019 Qin Leo. All rights reserved.
+//
+
+import WatchKit
+
+class SongTableRowController: NSObject {
+    @IBOutlet weak var nameLabel: WKInterfaceLabel!
+}
